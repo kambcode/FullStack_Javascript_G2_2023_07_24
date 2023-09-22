384ac61a86697da84684d0c05f399c7c248c4d10
GERMAN ARCINIEGAS <germanarciniegas@MacBook-Air-de-GERMAN.local>
2023-09-22 14:15:16 -0500

    Clase 8 Tarea

---
+++ MODULO_2/JS-C8/Tarea/tarea.tsx
@@ -0,0 +1,17 @@
+//// Palindromo
+//// ana ana   "ana ana"
+///// Crear una funcion que reciba una palabra o una frase y
+/// retorne si la entrada es palindroma
+
+///Input: "ana" OuTpUT: "Si la palabra/oracion es palindroma"
+///Input: "ana y  ana" OuTpUT: "Si la palabra/oracion es palindroma"
+///Input: "german" OuTpUT: "No la palabra/oracion no es palindroma"
+///Input: "german german" OuTpUT: "No la palabra/oracion no es palindroma"
+
+////Vamos a leer un poco acerca de recursividad
+////Google Utiliza la recursividad para sus motores de busqueda
+//// Ejemplos 1 2 3 5 8 Fibo
+
+///Input: 5 OutPut: 8
+///Input: 2 OutPut: 2
+///Algoritmpo de recursividad

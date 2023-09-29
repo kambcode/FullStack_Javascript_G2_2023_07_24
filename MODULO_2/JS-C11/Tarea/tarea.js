2ad3ba1e993d47669911c0f3cd3b38cd54cbbd66
GERMAN ARCINIEGAS <germanarciniegas@MacBook-Air-de-GERMAN.local>
2023-09-28 21:20:14 -0500

    Clase 11 video

--- MODULO_2/JS-C11/Tarea/tarea.js
+++ MODULO_2/JS-C11/Tarea/tarea.js
@@ -1,26 +1,13 @@
-/// Ejecutar en node js
-///Vamos a generar un metodo de espera utilizando promesas y setTimouts
 
-/// Vamos a imprimir Inicio mi metodo de espera
-/// Esperamos 4 S
-/// Vamos a imprimir mi primera etapa
-/// Esperamos 5 S
-/// Vamos a imprimir mi segunda etapa
-/// Esperamos 5 S
-/// Vamos a imprimir mi tercera etapa
-/// Esperamos 5 S
-/// Vamos a imprimir mi cuarta etapa
+/// Utilice el semaforo hecho en clase, e intente replicar el siguiente moviento.
 
-//Nota: Cada etapa es una funcion que recibe los parametros res y rej de una promesa 
-
-
-///Reutilizando las funciones Vamos a generar un metodo de espera
-/// Vamos a imprimir Inicio mi metodo de espera
-/// Esperamos 4 S
-/// Vamos a imprimir mi segunda etapa
-/// Esperamos 5 S
-/// Vamos a imprimir mi segunda etapa
-/// Esperamos 5 S
-/// Vamos a imprimir mi cuarta etapa
-/// Esperamos 5 S
-/// Vamos a imprimir mi primera etapa
+/// Inicio Rojo y Naranja prenden.
+/// Se apagan los anteriores (reset)
+/// Se prende amarillo y negro
+/// Se apagan los anteriores (reset)
+/// Se prende verde y violeta
+/// Se apagan los anteriores (reset)
+/// Se prende amarillo y negro
+/// Se apagan los anteriores (reset)
+/// Inicio Rojo y Naranja prenden.
+// y se repite el ciclo
--- README.md
+++ README.md
@@ -44,3 +44,4 @@ Modulo 2
  - 20/09/2023 - C8-Clousure, Sincronismo, Asincronismo, CallBacks https://youtu.be/XLg8n1qoqeE
  - 25/09/2023 - C9-CalBacks, TimeOuts, Node.js https://youtu.be/sNlCkq5WoBs
  - 27/09/2023 - C10-Promises, TimeOuts, Sync Await https://youtu.be/3Y9OR2qg9wQ
+ - 28/09/2023 - C11-Manejo del DOM https://youtu.be/nh4tfDV4PY8

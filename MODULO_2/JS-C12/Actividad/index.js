257f205deec27271d46a8f3b46adb5b76a234bd2
GERMAN ARCINIEGAS <germanarciniegas@MacBook-Air-de-GERMAN.local>
2023-10-02 21:36:08 -0500

    Clase 12

---
+++ MODULO_2/JS-C12/Actividad/index.js
@@ -0,0 +1,39 @@
+////// Repositorio FullStack_Javascript_G2_2023_07_24 ...
+
+//Es basicamente una carpeta
+
+
+///// Rama
+
+/// base de github, o es un hilo con un consecutivo de versiones, el orden es importante
+
+///Rama tiene un nombre master, o se llama main 
+
+
+////Versiones, commits
+
+//// Nombre: en git se llama commit
+//// Numero:  commit tiene algo especifico se llama numero de commit
+//// Author, nombre, email, informa cion del equipo
+
+//Varios comandos en git: 
+// cuales branch hay en el proyecto => git branch
+/// ver la historia del branch actual => git log
+/// viajar en la historia => git checkout
+/// que me ayuda a ver los estados => git status
+
+// Stados en git
+
+///1. Cambio red
+///2. Stage verde
+///3. Commit nombre o numero creamos una nueva version
+
+
+///Subir a la nube
+/// git push
+
+///Bajar a la nube
+/// git pull
+
+
+

db16bc259b0393a9fa85336e3f43550958eb2dcc
LOZANO <danstev50@gmail.com>
2023-12-11 20:20:51 -0500

    module 4 class 3

--- MODULO_4/BK_C3/src/rest.js
+++ MODULO_4/BK_C3/src/rest.js
@@ -2,13 +2,22 @@ import http from "http";
 
 const port = 3600;
 
+const users = [{}]
+
+const books = [{}]
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
+
+
+  //switch
 
   if (req.url === "/users") {
     res.write("users");
     res.end();
   }
+
+
 });
 
 server.listen(port, () => {
